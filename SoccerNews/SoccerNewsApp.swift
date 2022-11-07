f51d79d29e1f9c2f35d9e37958d50395a943d7e9
Yan Oliveira <oliveira.yan02@gmail.com>
2022-11-07 12:40:47 -0300

    Initial Commit

---
+++ SoccerNews/SoccerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoccerNewsApp.swift
+//  SoccerNews
+//
+//  Created by Yan Oliveira on 07/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SoccerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
