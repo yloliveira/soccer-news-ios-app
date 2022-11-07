cf7d9e91e78d98d3604432724ecc8ae62279ec12
Yan Oliveira <oliveira.yan02@gmail.com>
2022-11-07 12:55:20 -0300

    refactor: rename file

---
+++ SoccerNews/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SoccerNews
+//
+//  Created by Yan Oliveira on 07/11/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
+
+struct HomeView_Previews: PreviewProvider {
+  static var previews: some View {
+    HomeView()
+  }
+}
